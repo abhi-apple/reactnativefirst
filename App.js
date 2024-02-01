169149e30fb1b98f018b379f0f3711536abc2cd3
Abhinay Prakash Reddy <abhinayprakashreddy@Abhinays-MacBook-Air.local>
2024-02-01 11:59:25 +0530

    initial

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {
+  View,
+  ScrollView,
+  Image,
+  Button,
+  Text,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+const img = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor: "black",
+        flex: 1,
+        padding: 60,
+      }}
+    >
+      <Button
+        title="press"
+        onPress={() => {
+          console.log("Button pressed");
+        }}
+        color="lightgreen"
+      />
+      <Pressable
+        onPress={() => {
+          console.log("image pressed");
+        }}
+      >
+        <Image source={img} style={{ width: 300, height: 300 }}></Image>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          console.log("text pressed");
+        }}
+      >
+        <Text style={{ color: "white" }}>
+          lorem Ips incorrectly formatted incorrectly in the first place
+          (http://www.w3.org/TR/ Unicode Standardlorem Ips incorrectly formatted
+          incorrectly in the first place (http://www.w3.org/TR/ Unicode
+          Standardlorem Ips incorrectly formatted incorrectly in the first place
+          (http://www.w3.org/TR/ Unicode Standardlorem Ips incorrectly formatted
+          incorrectly in the first place (http://www.w3.org/TR/ Unicode
+          Standardlorem Ips incorrectly formatted incorrectly in the first place
+          (http://www.w3.org/TR/ Unicode Standardlorem Ips incorrectly formatted
+          first place (http://www.w3.org/TR/ Unicode Standard
+        </Text>
+      </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
